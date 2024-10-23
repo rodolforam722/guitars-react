aacb50dd80da90bbb4de81df2c0507553b1aacd9
Rodolfo <fofocr7222@gmail.com>
2024-10-23 13:37:37 -0600

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   
 
     <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
+        <h2 className="text-center">Hola</h2>
 
         <div className="row mt-5">
           {data.map((guitar) => (
